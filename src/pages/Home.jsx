0fa9b689a647799b9bd28e5b7567017a4c0c6c86
Tyler Gener <tylergen33@gmail.com>
2023-06-05 11:34:59 -0700

    Removed navbar and announcement from Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,5 @@
-import React from "react";
-import Announcement from "../components/Announcement";
 import Categories from "../components/Categories";
 import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
 import Newsletter from "../components/Newsletter";
 import Products from "../components/Products";
 import Slider from "../components/Slider";
@@ -10,8 +7,6 @@ import Slider from "../components/Slider";
 const Home = () => {
   return (
     <div>
-      <Announcement />
-      <Navbar />
       <Slider />
       <Categories />
       <Products />
